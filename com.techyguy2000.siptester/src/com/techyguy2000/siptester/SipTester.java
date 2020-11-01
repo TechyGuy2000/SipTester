6b85bfab2f2231cf7f9ed55d2e160692e6dc9cf8
Greg Bolton <greg.bolton.28@gmail.com>
2020-11-01 14:50:32 -0600

    Initial Commit

---
+++ com.techyguy2000.siptester/src/com/techyguy2000/siptester/SipTester.java
@@ -0,0 +1,17 @@
+package com.techyguy2000.siptester;
+
+/**
+ * @author gregb
+ *
+ */
+public class SipTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("SipTester");
+
+	}
+
+}
